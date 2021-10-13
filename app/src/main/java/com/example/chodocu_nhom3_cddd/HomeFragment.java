3a6e8f81e6027a8bcbe5c46cc5c904285299df24
acysan96 <thangduyduyduyduy@gmail.com>
2021-10-13 10:23:17 +0700

    13/10/2021 navigation bottom

---
+++ app/src/main/java/com/example/chodocu_nhom3_cddd/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.chodocu_nhom3_cddd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
