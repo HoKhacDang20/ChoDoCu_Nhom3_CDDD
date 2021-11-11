f8d0485d4cd5c2b9a191168c4c0f9185f246710a
Admin <hokhacdang2001@gmail.com>
2021-11-11 14:54:50 +0700

    HomeViewModel

---
+++ app/src/main/java/com/example/chodocu_nhom3_cddd/ui/home/HomeViewModel.java
@@ -0,0 +1,18 @@
+package com.example.chodocu_nhom3_cddd.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
