6383900117e5bf50a4992078c1fc6e1af6c2ae15
havi1234 <50940997+havi1234@users.noreply.github.com>
2021-11-11 16:12:13 +0700

    HaVi tạo bottom navigation

---
+++ app/src/main/java/com/example/chodocu_nhom3_cddd/UserMainActivity.java
@@ -0,0 +1,37 @@
+package com.example.chodocu_nhom3_cddd;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.chodocu_nhom3_cddd.databinding.UserMainLayoutBinding;
+
+public class UserMainActivity extends AppCompatActivity {
+
+    private UserMainLayoutBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = UserMainLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_user_main_layout);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
