3645ea9600d5ead1bfd02edeaf68c0167233d210
Admin <hokhacdang2001@gmail.com>
2021-11-16 15:21:00 +0700

    Quản lý giao dịch

---
+++ app/src/main/java/com/example/chodocu_nhom3_cddd/Employee_Detail_Activity.java
@@ -0,0 +1,193 @@
+package com.example.chodocu_nhom3_cddd;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import com.example.chodocu_nhom3_cddd.data_models.UserData;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class Employee_Detail_Activity extends AppCompatActivity {
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    private String userName, userID;
+    private Intent intent;
+    private Button btnBack, btnHome, btnRemove;
+    private ImageView imgNV;
+    private TextView txtFullName, txtSDT, txtDiaChi, txtEmployee;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.chitiet_nhanvien);
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnHome = (Button) findViewById(R.id.btnHome);
+        btnRemove = (Button) findViewById(R.id.btnRemove);
+        imgNV = (ImageView) findViewById(R.id.imgNV);
+        txtFullName = (TextView) findViewById(R.id.txtFullName);
+        txtSDT = (TextView) findViewById(R.id.txtSDT);
+        txtDiaChi = (TextView) findViewById(R.id.txtDiaChi);
+        txtEmployee = (TextView) findViewById(R.id.txtEmployee);
+
+        btnBack.setOnClickListener(backClick);
+        btnHome.setOnClickListener(homeClick);
+        btnRemove.setOnClickListener(removeClick);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(getIntent().getExtras() != null) {
+            userName = getIntent().getExtras().getString("UserName");
+            userID = getIntent().getExtras().getString("UserID");
+            databaseReference.child("User").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    if (snapshot.getValue(UserData.class).getUserID().equals(userID)) {
+                        storageReference.child(snapshot.getValue(UserData.class).getImage() + ".png").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Glide.with(Employee_Detail_Activity.this).load(uri).into(imgNV);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                            }
+                        });
+                        txtFullName.setText(snapshot.getValue(UserData.class).getHoTen());
+                        txtSDT.setText(snapshot.getValue(UserData.class).getSoDienThoai());
+                        txtDiaChi.setText(snapshot.getValue(UserData.class).getDiaChi());
+                        if (snapshot.getValue(UserData.class).getPermission() == 3){
+                            txtEmployee.setText("Shipper");
+                        }
+                        else if (snapshot.getValue(UserData.class).getPermission() == 2) {
+                            txtEmployee.setText("Nhân viên");
+                        }
+                    }
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+    }
+
+    View.OnClickListener removeClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DialogInterface.OnClickListener dialogClick = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    switch (which) {
+                        case DialogInterface.BUTTON_POSITIVE:
+                            databaseReference.child("User").addChildEventListener(new ChildEventListener() {
+                                @Override
+                                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                                    if (snapshot.getValue(UserData.class).getUserID().equals(userID)) {
+                                        databaseReference.child("User").child(snapshot.getKey()).removeValue();
+                                    }
+                                }
+
+                                @Override
+                                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                                }
+
+                                @Override
+                                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                                }
+
+                                @Override
+                                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+                            //Intent intent = new Intent(v.getContext(), NhanVien_AdminActivity.class);
+                            intent.putExtra("UserName", userName);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                            finish();
+                            startActivity(intent);
+                            break;
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            return;
+                    }
+                }
+            };
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setMessage("Bạn chắc chắn muốn xóa nhân viên?").setNegativeButton("No",dialogClick).setPositiveButton("Yes",dialogClick).show();
+        }
+    };
+
+    View.OnClickListener homeClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(v.getContext(), AdminMainActivity.class);
+            intent.putExtra("UserName", userName);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            finish();
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener backClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+//            Intent intent = new Intent(v.getContext(), NhanVien_AdminActivity.class);
+//            intent.putExtra("UserName", userName);
+//            intent.putExtra("UserID", userID);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            finish();
+            //startActivity(intent);
+        }
+    };
+}
