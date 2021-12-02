4adf1688faedbf2e0a9168333b2880cf51d05ffe
havi1234 <50940997+havi1234@users.noreply.github.com>
2021-12-02 15:52:43 +0700

    HaVi làm chức năng lấy danh sách shipper

---
+++ app/src/main/java/com/example/chodocu_nhom3_cddd/ShipperDanhSachActivity.java
@@ -0,0 +1,118 @@
+package com.example.chodocu_nhom3_cddd;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+
+import com.example.chodocu_nhom3_cddd.adapter.ShipperAdapter;
+import com.example.chodocu_nhom3_cddd.data_models.UserData;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class ShipperDanhSachActivity extends AppCompatActivity {
+
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private ArrayList<UserData> userDataArrayList;
+    private GridView gridShipper;
+    private Button btnBack;
+    private Intent intent;
+    private String userName, userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.shipper_danh_sach_layout);
+
+        gridShipper = (GridView) findViewById(R.id.gridShipper);
+        btnBack = (Button) findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(backClick);
+
+        gridShipper.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //intent = new Intent(view.getContext(), ShipperDSDonGiaoShipperActivity.class);
+                intent.putExtra("UserName", userName);
+                intent.putExtra("UserID", userID);
+                intent.putExtra("ShipperID", userDataArrayList.get(position).getUserID());
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        userDataArrayList = new ArrayList<>();
+
+        if(getIntent().getExtras() != null){
+            userName = getIntent().getExtras().getString("UserName");
+            userID = getIntent().getExtras().getString("UserID");
+
+            databaseReference.child("User").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    if(snapshot.getValue(UserData.class).getPermission() == 3){
+                        userDataArrayList.add(snapshot.getValue(UserData.class));
+                    }
+                    shipperLoad();
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+    }
+
+    View.OnClickListener backClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+            //intent = new Intent(v.getContext(), ShipperMainActivity.class);
+            intent.putExtra("UserName", userName);
+            intent.putExtra("UserID", userID);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+        }
+    };
+
+    public void shipperLoad(){
+        ShipperAdapter shipperAdapter = new ShipperAdapter(ShipperDanhSachActivity.this, R.layout.shipper_adapter_layout, userDataArrayList);
+        gridShipper.setAdapter(shipperAdapter);
+    }
+}
